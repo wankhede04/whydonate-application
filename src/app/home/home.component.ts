d980b53954c97256a7c0170ed442d09a3e882bfe
wankhede04 <wankhedevijay04@gmail.com>
2020-11-01 18:51:48 +0530

    Implemented routing to details page.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { ICustomerDetails } from '../models/customer';
 import { Customer } from '../store/customer/customer.action';
 import { CustomerDetailsState } from '../store/customer/customer.state';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -14,10 +15,14 @@ export class HomeComponent implements OnInit {
   @Select(CustomerDetailsState.getCustomer)
   public customer$: Observable<ICustomerDetails[]>;
 
-  constructor(private store: Store) {
+  constructor(private store: Store, private router: Router) {
     this.store.dispatch(new Customer.Get());
   }
 
   ngOnInit(): void {
+  }
+
+  public showCustomerDetails(customerID: string) {
+    this.router.navigate(['details'], {queryParams: {id: customerID}});
   }
 }
