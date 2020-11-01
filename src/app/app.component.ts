02d89d304507d742dd423740f732545bbf63230e
wankhede04 <wankhedevijay04@gmail.com>
2020-11-01 16:18:57 +0530

    Refactored the pre loaded code.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'whydonate-application';
-}
+export class AppComponent {}
